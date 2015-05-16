82a00c105eb1cb7e6f1c874d70861955c7b75755
Sam Hiatt <samhiatt@gmail.com>
2015-05-15 19:09:34 -0700

    Added logging.

--- controller.py
+++ controller.py
@@ -8,16 +8,20 @@ light = milight.LightBulb(['white'])
 def dawn():
 	controller.send(light.warmness(100))
 	controller.send(light.brightness(70))
+	print("%s --- Lights set to dawn.\n" % str(datetime.datetime.now()))
 
 def sunrise():
         controller.send(light.brightness(100))
         controller.send(light.warmness(0))
+	print("%s --- Lights set to sunrise.\n" % str(datetime.datetime.now()))
 
 def sunset():
         controller.send(light.warmness(100))
         controller.send(light.brightness(100))
+	print("%s --- Lights set to sunset.\n" % str(datetime.datetime.now()))
 
 def dusk():
         controller.send(light.brightness(20))
         controller.send(light.warmness(0))
+	print("%s --- Lights set to dusk.\n" % str(datetime.datetime.now()))
 
