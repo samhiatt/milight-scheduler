1b62dd5dd1cb083f289fdc2ed60877fe69432393
Sam Hiatt <samhiatt@gmail.com>
2015-05-15 20:54:35 -0700

    Fixed scheduling.

--- schedule.py
+++ schedule.py
@@ -16,7 +16,6 @@ today = datetime.date.today()
 sun = city.sun(date=today, local=True)
 
 logfile.write("%s --- %s\n" % (city_name,str(datetime.datetime.now())))
-#print('%s\nDawn:    %s\nSunrise: %s\nNoon:    %s\nSunset:  %s\nDusk:    %s' % (city_name,str(sun['dawn']),str(sun['sunrise']),str(sun['noon']),str(sun['sunset']),str(sun['dusk'])))
 
 controller = milight.MiLight({'host': '192.168.42.100', 'port': 8899}, wait_duration=0)
 light = milight.LightBulb(['white'])
@@ -45,19 +44,24 @@ def dusk():
 	logfile.write("%s --- Lights set to dusk.\n" % str(datetime.datetime.now()))
 	logfile.flush()
 
-s = sched.scheduler(time.time, time.sleep)
-s.enterabs(sun['dawn'], 1, dawn, ())
-logfile.write("Scheduled dawn for    %s.\n" % str(sun['dawn']))
-s.enterabs(sun['sunrise'], 1, sunrise, ())
-logfile.write("Scheduled sunrise for %s.\n" % str(sun['sunrise']))
-s.enterabs(sun['sunset'], 1, sunset, ())
-logfile.write("Scheduled sunset for  %s.\n" % str(sun['sunset']))
-s.enterabs(sun['dusk'], 1, dusk, ())
-logfile.write("Scheduled dusk for    %s.\n" % str(sun['dusk']))
+def getEpoch(datetime):
+	return int(datetime.strftime("%s"))
 
-secs = (sun['dusk']-datetime.datetime.now(sun['dusk'].tzinfo)).seconds+1
+s = sched.scheduler(time.time, time.sleep)
+s.enterabs(getEpoch(sun['dawn']), 1, dawn, ())
+logfile.write("Scheduled dawn for    %s.\n" % str(sun['dawn']))
+s.enterabs(getEpoch(sun['sunrise']), 1, sunrise, ())
+logfile.write("Scheduled sunrise for %s.\n" % str(sun['sunrise']))
+s.enterabs(getEpoch(sun['sunset']), 1, sunset, ())
+logfile.write("Scheduled sunset for  %s.\n" % str(sun['sunset']))
+s.enterabs(getEpoch(sun['dusk']), 1, dusk, ())
+logfile.write("Scheduled dusk for    %s.\n" % str(sun['dusk']))
+s.run()
+
+secs = (sun['dusk']-datetime.datetime.now(sun['dusk'].tzinfo)).total_seconds()+1
 logfile.write("%s --- Sleeping for %s seconds.\n" % (str(datetime.datetime.now()),secs))
 logfile.flush()
-time.sleep(secs)
+if secs>1:
+	time.sleep(secs)
 logfile.write("%s --- Sun's down. \nDone.\n" % str(datetime.datetime.now()))
 logfile.close()
