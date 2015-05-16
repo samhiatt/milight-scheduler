30a2feb7da8064b0e99cd4aa8ea4b1a512b43948
Sam Hiatt <samhiatt@gmail.com>
2015-05-16 00:16:10 -0700

    new modes

--- controller.py
+++ controller.py
@@ -6,22 +6,31 @@ controller = milight.MiLight({'host': '192.168.42.100', 'port': 8899}, wait_dura
 light = milight.LightBulb(['white'])
 
 def dawn():
-	controller.send(light.warmness(50))
-	controller.send(light.brightness(70))
-	print("%s --- Lights set to dawn.\n" % str(datetime.datetime.now()))
+	controller.send(light.brightness(30))
+	controller.send(light.warmness(70))
+	print("%s --- Lights set to dawn." % str(datetime.datetime.now()))
 
 def sunrise():
         controller.send(light.brightness(100))
+        controller.send(light.warmness(70))
+	print("%s --- Lights set to sunrise." % str(datetime.datetime.now()))
+
+def noon():
+        controller.send(light.brightness(100))
         controller.send(light.warmness(0))
-	print("%s --- Lights set to sunrise.\n" % str(datetime.datetime.now()))
+	print("%s --- Lights set to noon." % str(datetime.datetime.now()))
 
 def sunset():
-        controller.send(light.warmness(100))
         controller.send(light.brightness(100))
-	print("%s --- Lights set to sunset.\n" % str(datetime.datetime.now()))
+        controller.send(light.warmness(100))
+	print("%s --- Lights set to sunset." % str(datetime.datetime.now()))
 
 def dusk():
-        controller.send(light.brightness(20))
+        controller.send(light.brightness(40))
         controller.send(light.warmness(100))
-	print("%s --- Lights set to dusk.\n" % str(datetime.datetime.now()))
+	print("%s --- Lights set to dusk." % str(datetime.datetime.now()))
 
+def night():
+        controller.send(light.brightness(10))
+        controller.send(light.warmness(100))
+	print("%s --- Lights set to night." % str(datetime.datetime.now()))
--- schedule.py
+++ schedule.py
@@ -3,6 +3,7 @@ import datetime, os
 from astral import Astral
 import sched, time
 import milight
+import controller
 
 logfile = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'schedule.log'),'a')
 
@@ -17,31 +18,34 @@ sun = city.sun(date=today, local=True)
 
 logfile.write("%s --- %s\n" % (city_name,str(datetime.datetime.now())))
 
-controller = milight.MiLight({'host': '192.168.42.100', 'port': 8899}, wait_duration=0)
-light = milight.LightBulb(['white'])
-
 def dawn():
-	controller.send(light.warmness(50))
-	controller.send(light.brightness(70))
+	controller.dawn()
 	logfile.write("%s --- Lights set to dawn.\n" % str(datetime.datetime.now()))
 	logfile.flush()
 
 def sunrise():
-        controller.send(light.brightness(100))
-        controller.send(light.warmness(0))
+	controller.sunrise()
 	logfile.write("%s --- Lights set to sunrise.\n" % str(datetime.datetime.now()))
 	logfile.flush()
 
+def noon():
+	controller.noon()
+	logfile.write("%s --- Lights set to noon.\n" % str(datetime.datetime.now()))
+	logfile.flush()
+
 def sunset():
-        controller.send(light.warmness(100))
-        controller.send(light.brightness(100))
+	controller.sunset()
 	logfile.write("%s --- Lights set to sunset.\n" % str(datetime.datetime.now()))
 	logfile.flush()
 
 def dusk():
-        controller.send(light.brightness(20))
-        controller.send(light.warmness(100))
+	controller.dusk()
 	logfile.write("%s --- Lights set to dusk.\n" % str(datetime.datetime.now()))
+	logfile.flush()
+
+def night():
+	controller.night()
+	logfile.write("%s --- Lights set to night.\n" % str(datetime.datetime.now()))
 	logfile.flush()
 
 def getEpoch(datetime):
@@ -52,16 +56,23 @@ s.enterabs(getEpoch(sun['dawn']), 1, dawn, ())
 logfile.write("Scheduled dawn for    %s.\n" % str(sun['dawn']))
 s.enterabs(getEpoch(sun['sunrise']), 1, sunrise, ())
 logfile.write("Scheduled sunrise for %s.\n" % str(sun['sunrise']))
+s.enterabs(getEpoch(sun['noon']), 1, noon, ())
+logfile.write("Scheduled noon for    %s.\n" % str(sun['noon']))
 s.enterabs(getEpoch(sun['sunset']), 1, sunset, ())
 logfile.write("Scheduled sunset for  %s.\n" % str(sun['sunset']))
 s.enterabs(getEpoch(sun['dusk']), 1, dusk, ())
 logfile.write("Scheduled dusk for    %s.\n" % str(sun['dusk']))
+nighttime = sun['dusk']+datetime.timedelta(hours=1)
+s.enterabs(getEpoch(nighttime), 1, night, ())
+logfile.write("Scheduled night for   %s.\n" % str(nighttime))
+
 s.run()
 
-secs = (sun['dusk']-datetime.datetime.now(sun['dusk'].tzinfo)).total_seconds()+1
-logfile.write("%s --- Sleeping for %s seconds.\n" % (str(datetime.datetime.now()),secs))
-logfile.flush()
-if secs>1:
+secs = (nighttime-datetime.datetime.now(nighttime.tzinfo)).total_seconds()+1
+if secs>100000:
+	logfile.write("%s --- Sleeping for %s seconds.\n" % (str(datetime.datetime.now()),secs))
+	logfile.flush()
 	time.sleep(secs)
-logfile.write("%s --- Sun's down. \nDone.\n" % str(datetime.datetime.now()))
+
+logfile.write("%s --- Sun's down. Done.\n" % str(datetime.datetime.now()))
 logfile.close()
